36e24ad4fc77a3c5a4d370e88d68c1184f92e4fc
Beatriz Nishi <beatriz.nishi@gmail.com>
2022-10-28 11:58:12 -0300

    Aula2 code 1, 2 e 3

--- PDI/Aula_2/code3.py
+++ PDI/Aula_2/code3.py
@@ -2,11 +2,12 @@
 # Import Gaussian filter
 from skimage.filters import gaussian
 from PDI.src.pdi_utils import load_building_image,show_image
+
 building_image = load_building_image()
 
 
 # Apply filter sigma = 1
-gaussian_image = gaussian(___, sigma=___,multichannel = True)
+gaussian_image = gaussian(building_image, sigma=1,multichannel = True)
 
 # Show original and resulting image to compare
 show_image(building_image, "Original")
@@ -14,13 +15,13 @@ show_image(gaussian_image, "Reduced sharpness Gaussian with sigma = 1")
 
 
 # Apply gaussian filter sigma = 5
-____
+gaussian_image = gaussian(building_image, sigma=5,multichannel = True)
 
 # Show resulting image to compare
-show_image(___, "Reduced sharpness Gaussian with sigma = 5")
+show_image(gaussian_image, "Reduced sharpness Gaussian with sigma = 5")
 
 # Apply filter sigma = 10
-____
+gaussian_image = gaussian(building_image, sigma=10,multichannel = True)
 
 # Show resulting image to compare
-show_image(___, "Reduced sharpness Gaussian with sigma = 10")
+show_image(gaussian_image, "Reduced sharpness Gaussian with sigma = 10")
